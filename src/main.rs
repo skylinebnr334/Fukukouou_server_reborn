d2c7638d670ac3dd317614b796ef17c8f84e37a3
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-05-05 16:09:08 +0900

    added page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main(){
-    println!("hello world");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn rootpage()->impl Responder{
+    HttpResponse::Ok().body("root page")
+}
+
+#[actix_web::main]
+async fn main()->std::io::Result<()>{
+    HttpServer::new(|| {
+        App::new()
+            .service(rootpage)
+    })
+        .bind(("127.0.0.1", 8080))?
+    .run()
+        .await
 }

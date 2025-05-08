5fc430479edd5d69827c85d5cc536506ec8ab733
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-05-08 11:40:29 +0900

    fx

--- src/model_round1.rs
+++ src/model_round1.rs
@@ -1,6 +1,6 @@
 use diesel::sql_types::Integer;
 
-pub struct Round1_Data_Column{
+pub struct Round1DataColumn {
     pub id: Integer,
     pub team1 : Integer,
     pub team2 : Integer,
@@ -8,4 +8,14 @@ pub struct Round1_Data_Column{
     pub  team4 : Integer,
     pub  team5 : Integer,
     pub  team6 : Integer,
+}
+pub struct Round1IndexRound {
+    pub id: Integer,
+    pub current_stage : Integer,
+}
+pub struct Round1ScoreConfigDataColumn {
+    pub id:Integer,
+    pub correct:Integer,
+    pub miss:Integer,
+    pub ask_throw:Integer,
 }

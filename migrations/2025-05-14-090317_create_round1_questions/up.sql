79fdd93a878f4e7e2bd02adf5fb3a368e217921e
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-05-14 18:28:02 +0900

    addded

--- migrations/2025-05-14-090317_create_round1_questions/up.sql
+++ migrations/2025-05-14-090317_create_round1_questions/up.sql
@@ -1,1 +1,8 @@
 -- Your SQL goes here
+CREATE TABLE round1_questions (
+                             stageno INTEGER UNIQUE NOT NULL UNIQUE,
+    question TEXT,
+                             answer TEXT,
+    comment TEXT,
+                             PRIMARY KEY (stageno)
+);

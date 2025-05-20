e099c60d60fd01e621ada9f705e4c5aa18738da9
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-05-20 13:50:47 +0900

    F

--- src/model_round1_questions.rs
+++ src/model_round1_questions.rs
@@ -1,3 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Insertable, Deserialize, Serialize, Clone)]
+#[diesel(table_name = round1_questions)]
 pub struct Round1QuestionDataColumn{
     pub stageno:i32,
     pub question:String,

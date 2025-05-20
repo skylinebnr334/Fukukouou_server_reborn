9758a62adbd585f8479e4ed40db6624eb40bce76
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-05-20 14:22:48 +0900

    fx

--- src/api/route.rs
+++ src/api/route.rs
@@ -1,9 +1,15 @@
 mod Round1Server;
 use utoipa_swagger_ui::SwaggerUi;
 use actix_web::web;
+use utoipa::OpenApi;
 
+#[derive(OpenApi)]
+struct Api_Doc;
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.configure(
         Round1Server::Round1config
+    ).service(
+        SwaggerUi::new("/swagger-ui/{_:.*}")
+            .url("/api-doc/opanapi.json", Api_Doc::openapi()),
     );
 }

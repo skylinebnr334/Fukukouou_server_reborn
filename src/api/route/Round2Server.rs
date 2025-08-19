a66fe0720f146dab89159de62226208c311016d5
skylinebnr334 <168154498+skylinebnr334@users.noreply.github.com>
2025-08-19 16:43:50 +0900

    fx

---
+++ src/api/route/Round2Server.rs
@@ -0,0 +1,28 @@
+
+use actix::Addr;
+use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder};
+use actix_web_actors::ws;
+use crate::{db, schema};
+use crate::model_round2::{Round2DataColumn, Round2DataReturnStruct};
+
+use diesel::{QueryDsl, RunQueryDsl};
+
+#[utoipa::path(
+    get,
+    path="/Server2/round_datas",
+    responses(
+        (status = 200, description = "Get Round2 Data", body = Round2DataReturnStruct),
+        (status = 500, description = "Internal error")
+    ),
+)]
+#[get("/round_datas")]
+async fn getRoundDatasR2(db:web::Data<db::Pool>)->impl Responder{
+    let mut conn=db.get().unwrap();
+    let rows=schema::round2_data::table
+        .load::<Round2DataColumn>(&mut conn)
+        .expect("Error loading round1 data");
+    let return_obj=Round2DataReturnStruct{
+        result_data:rows,
+    };
+    HttpResponse::Ok().json(web::Json(return_obj))
+}
--- src/model_round2.rs
+++ src/model_round2.rs
@@ -1,6 +1,7 @@
 use crate::schema::round2_data;
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
+
 #[derive(Queryable, Insertable, Deserialize, Serialize, Clone,ToSchema)]
 #[diesel(table_name = round2_data)]
 pub struct Round2DataColumn{
@@ -8,4 +9,9 @@ pub struct Round2DataColumn{
     pub current_phase:i32,
     pub latest_down_num:i32,
     pub miss_timing:i32,
+}
+#[derive(Queryable, Deserialize, Serialize,ToSchema)]
+
+pub struct Round2DataReturnStruct{
+    pub result_data:Vec<Round2DataColumn>,
 }
